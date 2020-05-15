d63aeceb13d2c20db679a0a98cb49fedbb27520a
qiaowei <qiaowei@mychery.com>
2020-05-15 11:00:08 +0800

    Add the ideavimrc

--- backup.sh
+++ backup.sh
@@ -1,10 +1,16 @@
 # copy the vimrc to this path 
 cp ~/.vim/vimrc .vimrc
+
 # copy the zshrc to this path 
 cp ~/.zshrc .zshrc
+
 # copy the mardown config
 cp ~/.vim/md-snippets.vim md-snippets.vim
+
 # copy tmux config
 cp ~/.tmux.conf .tmux.conf
 cp ~/.tmux.conf.local .tmux.conf.local
+
+# copy the .ideavimrc to this path 
+cp ~/.ideavimrc .ideavimrc
 
